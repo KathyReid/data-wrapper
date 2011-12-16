17e495523c850c7dc4bc5d1f406687ef3a495936
Nicolas Kayser-Bril <n.kayserbril@gmail.com>
2011-12-16 15:18:35 +0100

    about page

--- views/contents.php
+++ views/contents.php
@@ -2,11 +2,12 @@
 	<div id="terms_of_use" class="popup">
 
 		<h1><?php echo _("Terms of use") ?></h1>
-
 		<h2><?php echo _("Fair usage policy") ?></h2>
-		<p><?php echo _("Be nice. You are authorized to use the service to produce visualizations that will be published on the web. You understand that you may not share your account nor indulge in any unlawful activity.") ?></p>
+		<p><?php echo _("DataWrapper is licensed to visualize data that comes from public, legal sources or your own research. Should we detect uses that might violate rights of organizations or single persons we reserve the right to block an account, notify the owner and ask them to stop using DataWrapper.") ?></p>
 		<h2><?php echo _("Limited responsibility") ?></h2>
 		<p><?php echo _("This service is provided as is. DataStory will certainly not be held accountable if your data is damaged by a server failure or any other cause.") ?></p>
+		<h2><?php echo _("Privacy") ?></h2>
+		<p><?php echo _("DataWrapper will not use your private data in any way not necessary for the provision of the Service. However, you have the right to ask for the modification or removal of any personal data in accordance with the laws of Germany.") ?></p>
 
 	</div>
 	<div id="impressum" class="popup">
@@ -14,9 +15,50 @@
 		<h1><?php echo _("Legal imprint") ?></h1>
 
 		<h2><?php echo _("Publisher") ?></h2>
-		<p><?php echo _(".") ?></p>
+		<p><?php echo _("ABZV • Bildungswerk der Zeitungen, 2011") ?></p>
 		<h2><?php echo _("Host") ?></h2>
-		<p><?php echo _(".") ?></p>
+		<p><?php echo _("CloudControl • Helmholtz-Str. 2-9, D-10587 Berlin") ?></p>
+		<h2><?php echo _("Credits") ?>
+		<p><?php echo _("Idea/Concept:") ?> Mirko Lorenz, <?php echo _("Development:") ?>: Nicolas Kayser-Bril, 2011</p>
+	</div>
+
+	<div id="about_datawrapper" class="popup">
+		
+		<h1><?php echo _("About") ?></h1>
+		<h3><?php echo _("What is DataWrapper?") ?></h3>
+
+		<p><b><?php echo _("In short: This is a little tool for journalists. It reduces the time to create and embed a simple chart from hours to seconds.") ?></b></p>
+
+		<h3><?php echo _("Motivation") ?></h3>
+		<?php echo _("There are many uses of data in journalism. The first step though is to use data more and often as a basis for reporting. Doing this has become easier, because of the large amounts of data becoming available, thanks to the OpenData movement and other initiatives.") ?></p>
+
+		<p><?php echo _("But there are bottlenecks.") ?></p>
+
+		<p><?php echo _("One is that creating even a simple visual chart and embedding it into a story is still too complex and time consuming. Yes, there are extensive other offerings, like IBM ManyEyes or the growing Google Chart API. They are great. But they have downsides, if used by journalists. For example you often have to store your trials data in public, can't get it out again. Plus, control over the look and feel of your charts is limited or complex to change if you are not a coder.") ?></p>
+
+		<h3><?php echo _("Create simple, embeddable charts in seconds, not hours") ?></h3>
+		<p><?php echo _("This is what DataWrapper does: This little tool reduces the time needed to create a correct chart and embed it into any CMS from hours to seconds.") ?></p>
+
+		<p><?php echo _("Furthermore, DataWrapper is not a honey-trap. The data you work with or store is yours and yours alone. Trials are not openly published.") ?></p> 
+
+		<p><?php echo _("On top of that, we encourage news organizations to fork DataWrapper via Github and install it on one of your own servers.") ?></p>
+		<p><?php echo _("The CSS is accessible, meaning that in one day you can make sure that the charts generated with DataWrapper have your logo, your visual styles and colours.") ?></p> 
+
+		<p><?php echo _("A short tutorial on how to use DataWrapper is here.") ?></p>
+
+		<h3><?php echo _("Background") ?></h3>
+
+		<p><?php echo _("DataWrapper was developed for ABZV, a journalism training organization affiliated to BDVZ (German Association of Newspaper Publishers). It is part of our efforts to develop a comprehensive curriculum for data-driven journalism.") ?></p>
+
+		<h3><?php echo _("Features") ?></h3>
+
+		<p><?php echo _("Use of this tool is free.") ?></p>
+
+		<p><?php echo _("DataWrapper 0.1 is a non-commercial, open source software, licensed under the MIT License.") ?></p>
+
+		<p><?php echo _("DataWrapper uses HTML5 Javascript libraries, namely Highcharts and D3.js, with more to come.") ?></p>
+
+		<p><?php echo _("DataWrapper can be forked on GitHub and installed on your own server.") ?></p>
 
 	</div>
 </div>
--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,12 @@
 
 <div id="footer">
+	<a href="#about_datawrapper" class="fancybox"><?php echo _("About") ?></a>
+	<span class=".separator"> • </span>
 	<a href="#terms_of_use" class="fancybox"><?php echo _("Terms of Use") ?></a>
 	<span class=".separator"> • </span>
 	<a href="#impressum" class="fancybox"><?php echo _("Legal imprint") ?></a>
+	<span class=".separator"> • </span>
+	<a href="https://github.com/n-kb/Data-Story"><?php echo _("Fork me on GitHub!") ?></a>
 	<?php
 
 	//Loads the divs that will appear in the fancybox
