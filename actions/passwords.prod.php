1373fd76cd3975fec9eb6436bd1289f16107864b
Nicolas Kayser-Bril <n.kayserbril@gmail.com>
2012-04-07 19:17:39 +0200

    changed pwd file

--- actions/passwords.prod.php
+++ actions/passwords.prod.php
@@ -1,18 +1,40 @@
 <?php
-	
+
+	// read the credentials file
+	$string = file_get_contents($_ENV['CRED_FILE'], false);
+	if ($string == false) {
+	    die('FATAL: Could not read credentials file');
+	}
+
+	// the file contains a JSON string, decode it and return an associative array
+	$creds = json_decode($string, true);
+
 	//DB credentials
-	define("DW_HOST", "127.0.0.1");
-	define("DW_DATABASE", "depjrkhynun");
-	define("DW_USERNAME", "depjrkhynun");
-	define("DW_PASSWORD", "ubMCeQSHp6Eu4Ge");
+	define("DW_HOST", $creds['MYSQLS']['MYSQLS_HOSTNAME']);
+	define("DW_DATABASE", $creds['MYSQLS']['MYSQLS_DATABASE']);
+	define("DW_USERNAME", $creds['MYSQLS']['MYSQLS_USERNAME']);
+	define("DW_PASSWORD", $creds['MYSQLS']['MYSQLS_PASSWORD']);
 
-	//To be used in Amazon Simple Email Service
-	define("AWS_SECRET", "a45aj1jGNJIwkrmI6nLy4mcvCIDNMlpnFSqPwg7W");
-	define("AWS_ACCESS_KEY", "1JFWSJFZN75V3AY4MAR2");
 
-	//gets the base URL to use in the app
-	define("BASE_DIR", "http://datawrapper.de");
+	//The following global vars are stored in the option table of the MYSQL database
+	//AWS_SECRET		To be used in Amazon Simple Email Service
+	//AWS_ACCESS_KEY	To be used in Amazon Simple Email Service
+	//BASE_DIR			Gets the base URL to use in the app
+	//PIWIK_PATH		Path to the piwik analytics server
 
-	//Piwik analytics
-	define("PIWIK_PATH", "www.datastory.de/piwik/");
+	$mysqli = new mysqli(DW_HOST,DW_USERNAME,DW_PASSWORD,DW_DATABASE);
+
+	//Queries the DB
+	$q = "SELECT * FROM options WHERE name='AWS_SECRET' OR name='AWS_ACCESS_KEY' OR name='BASE_DIR' OR name='PIWIK_PATH' ";
+
+	if ($result = $mysqli->query($q)) {
+
+		//fetches the result
+		while ($row = $result->fetch_object()) {
+
+			//Defines the global vars
+			define($row->name, $row->value);
+		}
+	}
+
 ?>
